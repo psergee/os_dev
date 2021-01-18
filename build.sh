1d55e3acb4519dbeeaac669f947ab0faade92752
psergee <pavel.sergeev@intel.com>
2021-01-19 00:41:15 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nasm loader.s -f bin -o loader.bin
-nasm extended_program.s -f bin -o extended_program.bin
+nasm ./src/boot/loader.s -f bin -o loader.bin -i ./src/boot
+nasm ./src/boot/extended_program.s -f bin -o extended_program.bin -i ./src/boot
 cat loader.bin extended_program.bin > disk.bin
